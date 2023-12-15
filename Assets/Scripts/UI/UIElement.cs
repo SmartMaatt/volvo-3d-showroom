41a870fa923b28b243cfcf8b2f78d4d311168557
Mateusz Płonka <43822422+SmartMaatt@users.noreply.github.com>
2023-12-15 23:59:43 +0100

    Bug Fix
    * Fixed duplicate UI scripts bugs
    * Fixed camera stacking bugs
    * Updated project to Unity 2020.3.48f1

--- Assets/Scripts/UI/UIElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIElement : MonoBehaviour
-{
-    public virtual void ReloadPanel() { }
-    public virtual void ReloadLanguage() { }
-}
